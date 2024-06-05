1728b557853cfb6cdb4e8c7b3b2bbb5d11fbd1e2
Fatma Ashraf <bashraf500@gmail.com>
2024-06-05 10:39:49 +0300

    try test serivce with instance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,12 @@ export class AppComponent {
   sayHello(){
     return 'Hello World!'
   }
+  logIn(){
+    localStorage.setItem('token','#')
+  }
+  logOut(){
+    localStorage.removeItem('token')
+  }
   // sayHello1(){
   //   return 'Hello World!'
   // }
---
+++ src/app/services/auth.service.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  //before every test they make instance new to before it func to reset the instance
+  //clean up the instance brfore each test
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AuthService);
+  });
+  afterEach(() => {
+    localStorage.removeItem('token');
+  });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+  it('must return true if there is a token in localStorage', () => {
+    localStorage.setItem('token', '#');
+    expect(service.isAuth()).toBeTruthy();
+  });
+});
